9723a24564ad3d6fc23ee45f3dfce68a26628f84
Emily H <157163774+emheyboer@users.noreply.github.com>
2026-01-12 14:20:43 -0500

    /stats command & better error handling

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const discord = require('discord.js');
 const config = require('./config.json');
 
+const stats = {};
+
 const client = new discord.Client({
     intents: [
     discord.GatewayIntentBits.DirectMessages,
@@ -12,13 +14,22 @@ client.once(discord.Events.ClientReady, (readyClient) => {
     console.log(`Logged in as ${JSON.stringify(readyClient.user.tag)}`);
 });
 
-client.on(discord.Events.InteractionCreate, (interaction) => {
+client.on(discord.Events.InteractionCreate, async (interaction) => {
     if (!interaction.isChatInputCommand()) return; 
     const command = interaction.commandName;
-    if (command == 'greet') {
-        interaction.reply(config.greet_msg);
-        const author = interaction?.member?.user;
-        console.log('greeted', JSON.stringify(`${author?.global_name} (@${author?.username})`));
+    const author = interaction?.member?.user;
+    switch (command) {
+        case 'greet':
+            interaction.reply(config.greet_msg);
+            console.log('greeted', JSON.stringify(`${author?.global_name} (@${author?.username})`));
+            break;
+        case 'stats':
+            if (!stats['sent']) {
+                await getStats();
+            }
+            interaction.reply(`i've received ${stats['sent'].toLocaleString()} messages this month`);
+            console.log('sent stats to', JSON.stringify(`${author?.global_name} (@${author?.username})`));
+            break;
     }
 });
 
@@ -45,8 +56,8 @@ ${'='.repeat(60)}`);
     }
 });
 
-function sendNotification(parameters) {
-    fetch('https://api.pushover.net/1/messages.json', {
+async function sendNotification(parameters) {
+    const response = await fetch('https://api.pushover.net/1/messages.json', {
         method: 'post',
         body: new URLSearchParams({
             token: config.pushover.token,
@@ -56,6 +67,38 @@ function sendNotification(parameters) {
             ...parameters
         }),
     });
+
+    const json = await response.json();
+    
+    const http_status = response.status;
+    const json_status = json.status;
+    const success = http_status == 200 && json_status == 1;
+    if (success) {
+        const headers = response.headers;
+        stats['limit'] = Number(headers['x-limit-app-limit']);
+        stats['remaining'] = Number(headers['x-limit-app-remaining']);
+        stats['reset'] = new Date(Number(headers['x-limit-app-reset']) * 1000);
+        stats['sent'] = stats['limit'] - stats['remaining'];
+        return;
+    } 
+    
+    console.error(JSON.stringify(json, null, 2));
+    if (http_status >= 400 && http_status < 500) {
+        console.error(`received ${http_status} error from pushover; shutting down...`);
+        process.exit(1);
+    } else {
+        console.error(`received ${http_status} error from pushover`);
+    }
+}
+
+async function getStats() {
+    const response = await fetch(`https://api.pushover.net/1/apps/limits.json?token=${config.pushover.token}`);
+    const json = await response.json();
+
+    stats['limit'] = json['limit'];
+    stats['remaining'] = json['remaining'];
+    stats['reset'] = json['reset'];
+    stats['sent'] = stats['limit'] - stats['remaining'];
 }
 
 const rest = new discord.REST().setToken(config.discord.token);
@@ -66,6 +109,12 @@ const commands = [
             discord.InteractionContextType.BotDM,
             discord.InteractionContextType.Guild
         ),
+    new discord.SlashCommandBuilder().setName('stats').setDescription('shows some stats')
+    .setContexts(
+        discord.InteractionContextType.PrivateChannel,
+        discord.InteractionContextType.BotDM,
+        discord.InteractionContextType.Guild
+    ),
 ];
 
 rest.put(discord.Routes.applicationCommands(config.discord.clientId), { body: commands });
